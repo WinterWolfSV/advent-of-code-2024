cac4b0a49b0bf06b7c9c8673c6fe7218f42b0479
WinterWolfSV <what112222@gmail.com>
2024-12-05 22:43:09 +0100

    Day 3

---
+++ src/D04/D04P2.java
@@ -0,0 +1,38 @@
+package D04;
+
+import util.Util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class D04P2 {
+    public static void main(String[] a) {
+        String data = Util.fileToString("src/D04/input.txt");
+        String[] splitData = data.split("\n");
+        int rows = splitData.length;
+        int cols = splitData[0].length();
+        int tot = 0;
+        for (int row = 1; row < rows - 1; row++) {
+            for (int col = 1; col < cols - 1; col++) {
+                if (isXMas(splitData, row, col)) {
+                    tot++;
+                }
+            }
+        }
+        System.out.println(tot);
+    }
+
+    public static boolean isXMas(String[] grid, int row, int col) {
+        try {
+            String[] smallGrid = {grid[row - 1].substring(col - 1, col + 2), grid[row].substring(col - 1, col + 2), grid[row + 1].substring(col - 1, col + 2)};
+            System.out.println(String.join("\n", smallGrid));
+            Pattern pattern = Pattern.compile("(?=M.{3}A.{3}S|S.{3}A.{3}M)(?=.{2}M.A.S|.{2}S.A.M).");
+            Matcher m = pattern.matcher(String.join("", smallGrid));
+            boolean find = m.find();
+            System.out.println(find ? "yes\n" : "no\n");
+            return find;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
