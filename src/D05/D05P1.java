24eb0976278828aee7d8d4b4f3bf94333afbb2bd
WinterWolfSV <what112222@gmail.com>
2024-12-05 22:44:02 +0100

    Day 5

---
+++ src/D05/D05P1.java
@@ -0,0 +1,30 @@
+package D05;
+
+import util.Util;
+
+public class D05P1 {
+    public static void main(String[] a) {
+        String[] sections = Util.fileToArray("src/D05/input.txt", "\n\n");
+        String[] rules = sections[0].split("\n");
+        String[] lists = sections[1].split("\n");
+        int tot = 0;
+        for (String l : lists) {
+            boolean flag = true;
+            for (String r : rules) {
+                int p1 = l.indexOf(r.split("\\|")[0]);
+                int p2 = l.indexOf(r.split("\\|")[1]);
+                if (p1 == -1 | p2 == -1 | p1 < p2) {
+                    System.out.println(p1 + " " + p2);
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                String[] larr = l.split(",");
+                tot += Integer.parseInt(larr[(larr.length - 1) / 2]);
+            }
+        }
+        System.out.println(tot);
+    }
+}
---
+++ src/D05/D05P2.java
@@ -0,0 +1,51 @@
+package D05;
+
+import util.Util;
+
+import java.util.*;
+
+public class D05P2 {
+    public static void main(String[] a) {
+        String data = Util.fileToString("src/D05/input.txt");
+        String[] sections = data.split("\n\n");
+
+        int tot = 0;
+
+        for (String s : sections[1].split("\n")) {
+            String[] cs = checkRules(s.split(","), sections[0].split("\n"));
+            if (Arrays.equals(cs, s.split(","))) {
+                System.out.println("We got a no-change!");
+            } else {
+                tot += Integer.parseInt(cs[(cs.length - 1) / 2]);
+                System.out.println("We got a change!");
+            }
+            System.out.println(Arrays.toString(cs));
+        }
+        System.out.println(tot);
+    }
+
+    public static String[] checkRules(String[] l, String[] rules) {
+        String[] retval = l;
+        List<String> sl = List.of(l);
+
+        for (String r : rules) {
+            int p1 = sl.indexOf(r.split("\\|")[0]);
+            int p2 = sl.indexOf(r.split("\\|")[1]);
+            if (!(p1 == -1 | p2 == -1 | p1 < p2)) {
+
+                String[] switched = switchPositions(l, p2, p1);
+                retval = checkRules(switched, rules);
+                break;
+            }
+        }
+
+        return retval;
+    }
+
+    public static String[] switchPositions(String[] l, int i1, int i2) {
+        String temp = l[i2];
+        l[i2] = l[i1];
+        l[i1] = temp;
+        return l;
+    }
+}
